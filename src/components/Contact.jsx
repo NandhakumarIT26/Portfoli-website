bdccd1d4bd9da072dd60fa698cc9a6507d4d30a3
NandhakumatIT26 <nandhakumaredu.it@gmail.com>
2025-12-04 13:41:32 +0530

    linkdin-resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -76,7 +76,7 @@ const Contact = () => {
                 <div>
                   <p className="text-sm text-gray-600 dark:text-gray-400">LinkedIn</p>
                   <a
-                    href="www.linkedin.com/in/nandhakumar-ap"
+                    href="www.linkedin.com/in/nandha-kumar-770356259"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-lg font-medium text-gray-800 dark:text-gray-200 hover:text-primary-500 transition-colors"
